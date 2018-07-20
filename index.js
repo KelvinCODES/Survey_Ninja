0319975f31ab28dcbc58c48ddf09d8283111103c
Kelvin Kong <kelvink.kong@mail.utoronto.ca>
2018-07-19 20:48:19 -0400

    second deploy

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
+//sd
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
